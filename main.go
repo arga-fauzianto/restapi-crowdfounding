383b28eb2c8110f3633e4722e3160ea498fd1238
arga-fauzianto <argafauzianto27@gmail.com>
2023-04-02 23:29:39 -0700

    updated

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"rest-crowdfounding/auth"
 	"rest-crowdfounding/handler"
@@ -25,8 +24,6 @@ func main() {
 	userService := user.NewService(userRepository)
 
 	authService := auth.NewService()
-
-	fmt.Println(authService.GenerateToken(1001))
 
 	userHandler := handler.NewUserHandler(userService, authService)
 
