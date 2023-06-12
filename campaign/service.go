bd1d7eed0f22fcb8977f3694e0203e1c81fb4c13
arga-fauzianto <argafauzianto27@gmail.com>
2023-06-12 10:11:52 +0700

    next update and test service

--- campaign/service.go
+++ campaign/service.go
@@ -1,7 +1,7 @@
 package campaign
 
 type Service interface {
-	GetCampaigns(userID int) ([]Campaign, error)
+	FindCampaigns(userID int) ([]Campaign, error)
 }
 
 type service struct {
@@ -12,19 +12,20 @@ func NewService(repository Repository) *service {
 	return &service{repository}
 }
 
-func (s *service) GetCampaigns(userId int) ([]Campaign, error) {
-	if userId != 0 {
-		campaigns, err := s.repository.FindByUserID(userId)
+func (s *service) FindCampaigns(userID int) ([]Campaign, error) {
+
+	if userID != 0 {
+
+		campaigns, err := s.repository.FindByUserID(userID)
 
 		if err != nil {
 			return campaigns, err
 		}
 
 		return campaigns, nil
-
 	}
 
-	campaigns, err := s.repository.FindByUserID(userId)
+	campaigns, err := s.repository.FindAll()
 
 	if err != nil {
 		return campaigns, err
