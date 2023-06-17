fa203f97ca3a368e37f8090e9aa80dce1e19526e
arga-fauzianto <argafauzianto27@gmail.com>
2023-06-17 11:45:04 +0700

    update create campaign repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -8,6 +8,7 @@ type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(UserID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
+	Save(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -48,6 +49,16 @@ func (r *repository) FindByID(ID int) (Campaign, error) {
 
 	var campaign Campaign
 	err := r.db.Preload("User").Preload("CampaignImages").Where("id = ?", ID).Find(&campaign).Error
+
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) Save(campaign Campaign) (Campaign, error) {
+	err := r.db.Create(&campaign).Error
 
 	if err != nil {
 		return campaign, err
